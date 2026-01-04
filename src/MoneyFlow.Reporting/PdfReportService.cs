d36627930d286412bb9312a913ffb7fb0dab1348
RogerAguiar <roger.silvaaguiar@yahoo.com.br>
2026-01-04 13:17:55 -0300

    It organizes the project structure.

---
+++ src/MoneyFlow.Reporting/PdfReportService.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoneyFlow.src.MoneyFlow.Reporting
+{
+    internal class PdfReportService
+    {
+    }
+}
--- src/MoneyFlow.UI/Program.cs
+++ src/MoneyFlow.UI/Program.cs
@@ -1,4 +1,4 @@
-namespace MoneyFlow
+namespace MoneyFlow.src.MoneyFlow.UI
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace MoneyFlow
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
